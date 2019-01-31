348f6c97eeb273aad7e73f09422ebc5de43d208e
Boris Hsu <Boris@HsuBorisdeMacBook-Air.local>
2019-02-01 02:08:26 +0800

    Update removing cache script

--- saving_my_128g_mac.py
+++ saving_my_128g_mac.py
@@ -3,23 +3,17 @@ import os
 import re
 import shutil
 
-TEMP_TRASH_PATH = '/Users/s1ro6/Desktop/_FuckingCaches_'
+TEMP_TRASH_PATH = os.path.expanduser('~/Desktop/_FuckingCaches_')
 
 
 # Remove JetBrain IDE's cache.¬
 def remove_jetbrain_cache():
-    BASE_PATH = '/Users/s1ro6/Library/Caches'
-
-    # Create temp trash dir
-    if not os.path.exists(TEMP_TRASH_PATH):
-        os.mkdir(TEMP_TRASH_PATH)
-
-    # Search cache and move cache
-    for name in os.listdir(BASE_PATH):
+    base_path = os.path.expanduser('~/Library/Caches')
+    for name in os.listdir(base_path):
         regex = r'^(WebStorm)\d{4}\.\d+'
         if re.match(regex, name):
-            path = os.path.join(BASE_PATH, name)
-            # shutil.move(path, TEMP_TRASH_PATH)
+            path = os.path.join(base_path, name)
+            delete_cache(path)
             print(os.path.abspath(path))
 
     # Open the trash bin with Finder.app
@@ -28,8 +22,23 @@ def remove_jetbrain_cache():
 
 # Remove Xcode cache.
 def remove_xcode_cache():
-    pass
+    # Remove Derived data
+    derived_path = os.path.expanduser('~/Library/Developer/Xcode/DerivedData')
+    for f in os.listdir(derived_path):
+        delete_cache(os.path.join(derived_path, f))
+        print(os.path.join(derived_path, f))
+
+    # Remove archives data
+    archives_path = os.path.expanduser('~/Library/Developer/Xcode/Archives')
+
+
+# Move the files to temp trash.
+def delete_cache(path):
+    if not os.path.exists(TEMP_TRASH_PATH):
+        os.mkdir(TEMP_TRASH_PATH)
+    shutil.move(path, TEMP_TRASH_PATH)
 
 
 if __name__ == '__main__':
-    remove_jetbrain_cache()
+    # remove_jetbrain_cache()
+    remove_xcode_cache()
