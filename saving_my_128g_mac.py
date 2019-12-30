7c500073bf177cf1809a18e15a57defe93f92e5f
s1ro6 <Joyreece@msn.com>
2019-12-30 15:33:55 +0800

    重新调整删除文件规则

--- saving_my_128g_mac.py
+++ saving_my_128g_mac.py
@@ -1,46 +1,29 @@
 #!/usr/bin/env python3
+#
 # Oh my god, Plz save my little brother 128GB MBP...
+#
 
 import os
-import re
-import shutil
 
-TEMP_TRASH_PATH = os.path.expanduser('~/Desktop/_FuckingCaches_')
-
-
-def remove_jetbrain_cache():
-    """ Remove JetBrain IDE's cache. """
-    base_path = os.path.expanduser('~/Library/Caches')
-    for name in os.listdir(base_path):
-        regex = r'^(WebStorm)\d{4}\.\d+'
-        if re.match(regex, name):
-            path = os.path.join(base_path, name)
-            delete_cache(path)
-            print(os.path.abspath(path))
-
-    # Open the trash bin with Finder.app
-    # os.system(f'open {TEMP_TRASH_PATH}')
-
-
-def remove_xcode_cache():
-    """ Remove the Xcode's removable caches. """
-    derived_path = os.path.expanduser('~/Library/Developer/Xcode/DerivedData')
-    # for f in os.listdir(derived_path):
-    # print(os.path.join(derived_path, f))
-    # delete_cache(os.path.join(derived_path, f))
-
-    archives_path = os.path.expanduser('~/Library/Developer/Xcode/Archives')
-    for f in os.listdir(archives_path):
-        print(os.path.join(archives_path, f))
-
-
-# Move the files to temp trash.
-def delete_cache(path):
-    if not os.path.exists(TEMP_TRASH_PATH):
-        os.mkdir(TEMP_TRASH_PATH)
-    shutil.move(path, TEMP_TRASH_PATH)
+def remove_file(path):
+    if os.path.isdir(path):
+        for name in os.listdir(path):
+            sub_path = os.path.join(path, name)
+            remove_file(sub_path)
+    else:
+        os.remove(path)
 
 
 if __name__ == '__main__':
-    # remove_jetbrain_cache()
-    remove_xcode_cache()
+    path_list = [
+        '~/Library/Developer/Xcode/DerivedData',
+        '~/Library/Developer/Xcode/iOS DeviceSupport',
+        '~/Library/Developer/Xcode/iOS Device Logs',
+    ]
+
+    for p in path_list:
+        path = os.path.expanduser(p)
+        if os.path.exists(path):
+            remove_file(path)
+
+    print('Done!')
