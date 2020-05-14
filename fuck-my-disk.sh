fa0a1ad3e10e0f05037b63951390f24ba5bdc7f3
s1ro6 <Joyreece@msn.com>
2020-05-14 11:04:04 +0800

    Optimize code

--- fuck-my-disk.sh
+++ fuck-my-disk.sh
@@ -4,59 +4,80 @@
 
 TOTAL_CACHE_SIZE=0
 
-function removeFiles() {
-    for DIR_PATH in "$@"; do
-        if [[ -d $DIR_PATH ]]; then
-            DIR_SIZE=$(du -ks "$DIR_PATH" | awk '{print $1}')
-            TOTAL_CACHE_SIZE=$((TOTAL_CACHE_SIZE + DIR_SIZE))
-            echo "$DIR_SIZE KB [$DIR_PATH]"
-
-            mv "$DIR_PATH" "$HOME/.Trash/"
-        else
-            echo "The folder does not exist. [$DIR_PATH]"
+remove_cache_file() {
+    for dir_path in "$@"; do
+        if [[ -d $dir_path ]]; then
+            dir_size=$(du -ks "$dir_path" | awk '{print $1}')
+            TOTAL_CACHE_SIZE=$((TOTAL_CACHE_SIZE + dir_size))
+            echo "$dir_size KB [$dir_path]"
+            mv "$dir_path" "$HOME/.Trash/"
         fi
     done
 }
 
-function getHumanReadableResult() {
-    SIZE_NUMBER=$1
-    if [[ $SIZE_NUMBER -gt 1000000 ]]; then
-        SIZE_NUMBER="$((SIZE_NUMBER / 1000000)) GB"
-    elif [[ $SIZE_NUMBER -gt 1000 ]]; then
-        SIZE_NUMBER="$((SIZE_NUMBER / 1000)) MB"
+get_human_readable_result() {
+    size_number=$1
+    if [[ $size_number -gt 1000000 ]]; then
+        size_number="$((size_number / 1000000)) GB"
+    elif [[ $size_number -gt 1000 ]]; then
+        size_number="$((size_number / 1000)) MB"
     fi
-    echo -e "\nTotal cache size: \033[32m$SIZE_NUMBER\033[0m\n"
-    echo -e "Curren available space: $(df -H | grep "disk1s1" | awk '{print $4}')"
+    echo -e "\nRemoved cache size: \033[32m$size_number\033[0m"
+    echo -e "Previous available space: $(df -H | grep "disk1s1" | awk '{print $4}')"
 }
 
-function main() {
+# Fixed path caches
+remove_normal_caches() {
     CACHE_DIR_LIST=(
-        # QQ Caches
+        # Steel Drivers
+        "/Library/Application Support/SteelSeries Engine 3/updates"
+
+        # Tencent QQ Caches
         "$HOME/Library/Containers/com.tencent.qq/Data/Library/Caches/Avatar"
         "$HOME/Library/Containers/com.tencent.qq/Data/Library/Caches/Images"
         "$HOME/Library/Containers/com.tencent.qq/Data/Library/Caches/Videos"
         "$HOME/Library/Containers/com.tencent.qq/Data/Library/Caches/QQThumbnail"
 
-        # WeChat Caches
-        "$HOME/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/dec0ab91fa2da81feaff27e057044f2c/Stickers"
-
-        # WeChat Work Caches
-        "$HOME/Library/Containers/com.tencent.WeWorkMac/Data/Library/Application Support/WXWork/Data/1688852443323900/Avator"
-        "$HOME/Library/Containers/com.tencent.WeWorkMac/Data/Library/Application Support/WXWork/Data/1688852443323900/Cache"
-        "$HOME/Library/Containers/com.tencent.WeWorkMac/Data/Library/Application Support/WXWork/Data/1688852443323900/Emotion"
-
-        # Netease Music Caches
+        # Netease Cloud Music
         "$HOME/Library/Containers/com.netease.163music/Data/Library/Caches"
 
-        # Xcode Caches
+        # Apple Xcode Caches
         "$HOME/Library/Developer/Xcode/DerivedData"
         "$HOME/Library/Developer/Xcode/iOS DeviceSupport"
-
-        # Steel Drivers
-        "/Library/Application Support/SteelSeries Engine 3/updates"
     )
 
-    removeFiles "${CACHE_DIR_LIST[@]}"
-    getHumanReadableResult $TOTAL_CACHE_SIZE
+    remove_cache_file "${CACHE_DIR_LIST[@]}"
 }
+
+# Tencent WeChat
+# TODO: Optimize the way getting the cache path
+remove_wechat_caches() {
+    remove_cache_file "$HOME/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/dec0ab91fa2da81feaff27e057044f2c/Stickers"
+}
+
+# Tencent WeWork (aka WeChat Work Edition)
+remove_wechat_work_caches() {
+    root_data_path="$HOME/Library/Containers/com.tencent.WeWorkMac/Data/Library/Application Support/WXWork/Data"
+    root_data_subdir_list=$(ls "$root_data_path")
+
+    for random_dir_name in $root_data_subdir_list; do
+        random_dir_path="$root_data_path/$random_dir_name"
+        if [[ -d "$random_dir_path/Cache" ]]; then
+            cache_path_list=(
+                "$random_dir_path/Cache"
+                "$random_dir_path/Avator"
+                "$random_dir_path/Emotion"
+                "$HOME/Library/Containers/com.tencent.WeWorkMac/Data/Upgrade"
+            )
+            remove_cache_file "${cache_path_list[@]}"
+        fi
+    done
+}
+
+main() {
+    remove_normal_caches
+    remove_wechat_work_caches
+    get_human_readable_result $TOTAL_CACHE_SIZE
+}
+
 main
